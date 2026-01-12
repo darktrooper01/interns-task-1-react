46ed0f326d1a4955b67b8b129404842ecd6e7229
mahmoud <mahmoudmuhdmm@gmail.com>
2026-01-12 21:27:30 +0100

    Updates

---
+++ src/pages/Hero.jsx
@@ -0,0 +1,24 @@
+import './Hero.css'
+
+function Hero() {
+  return (
+     <>
+     
+      <div>
+        <h3>VERTEX</h3>
+        
+    </div>
+    
+    <div>
+        <p>Where Strategy meets design to create Scalable, high-performance <br />
+        Web products for the future.</p>
+    </div>
+     
+     
+     </>
+
+   
+  )
+}
+
+export default Hero
